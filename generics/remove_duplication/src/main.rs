66b7e5dd066c808bef53253d5472a3d9174c2174
daltondiaz <d_tecnologia@yahoo.com.br>
2022-08-29 21:16:07 -0300

    generics: create a new function to find the largest char

--- generics/remove_duplication/src/main.rs
+++ generics/remove_duplication/src/main.rs
@@ -9,15 +9,26 @@ fn largest(list: &[i32]) -> &i32 {
     largest
 }
 
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
     let result = largest(&number_list); 
 
-    println!("The largest numbei is {}", result);
+    println!("The largest number is {}", result);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    
-    let result = largest(&number_list);
-    println!("The largest numbei is {}", result);
+    let char_list = vec!['a', 'w', 'x', 'b', 'z']; 
+
+    let result =  largest_char(&char_list);
+    println!("The largest char is {}", result);
 }
